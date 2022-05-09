195f9c7801a94fb80258aebcc0055bd1cc1df880
Hongjun Wu <hongjunwu@hotmail.com>
2022-05-09 19:32:53 -0400

    more data

--- trackair-opla/data_logger.py
+++ trackair-opla/data_logger.py
@@ -3,9 +3,9 @@ import datetime
 
 today_date = datetime.date.today()
 
-arduino_port = "/dev/cu.usbmodem11301" #serial port of Arduino
+arduino_port = "/dev/cu.usbmodem21301" #serial port of Arduino
 baud = 115200 # arduino opla runs at 115200 baud
-fileName = "/Users/hongjunwu/Documents/GitHub/trackair-opla/trackair-opla/tata_226_20220509-18-52.csv" #name of the CSV file generated
+fileName = "/Users/hongjunwu/Documents/GitHub/trackair-opla/trackair-opla/house_20K_20220509-19-23.csv" #name of the CSV file generated
 
 ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
